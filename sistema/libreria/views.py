a8485f889d1119ca17c792b0b4baf4e8f230d774
Darwin <darwinquinnes735@gmail.com>
2022-03-14 17:45:05 -0500

    primer commit de la app

---
+++ sistema/libreria/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
+
+# Create your views here.
+def inicio(request):
+    return HttpResponse('<h1>I could do it<h1>')
+
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+
+def libros(request):
+    return render(request, 'libros/index.html')
