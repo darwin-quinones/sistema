ecbd9fd541d445c2fe433244313d75839602f879
darwinquinones <dquinonez3@misena.edu.co>
2022-03-16 13:03:00 -0500

    se hace un registro de libros

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Libro
 from .forms import LibroForm
@@ -15,8 +15,12 @@ def libros(request):
     return render(request, 'libros/index.html', {'libros': libros})
 
 def crear(request):
-    formilario = LibroForm(request.POST or None)
-    return render(request, 'libros/crear.html', {'formulario':formilario})
+    ## guardar datos incluye files
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/crear.html', {'formulario':formulario})
 
 def editar(request):
     return render(request, 'libros/editar.html')
