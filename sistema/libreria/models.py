29ea4481f32dbfbb5c110084571b5b4f9d332e0f
darwinquinones <dquinonez3@misena.edu.co>
2022-03-16 11:37:59 -0500

    se agrega una funcion para mostrar el titulo y la descripcion. funcion para eliminar

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -7,3 +7,13 @@ class Libro(models.Model):
     titulo = models.CharField(max_length=100, verbose_name='titulo')
     imagen = models.ImageField(upload_to='imagenes/', null=True, verbose_name="imagen")
     descripcion = models.TextField(null=True, verbose_name="descripción")
+    
+    def __str__(self):
+        fila = 'titulo: '+ self.titulo+ ' - ' + 'descripcion: '+ self.descripcion
+        return fila
+    
+    
+    # funcion para eliminar la foto 
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
